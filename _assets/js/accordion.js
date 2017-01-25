4eb71c1eb18aa3bef8564e17841e2327e2497a21
cassio mansini <cassiomansini@gmail.com>
2017-01-25 19:04:04 -0200

    ajustes home

---
+++ _assets/js/accordion.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#accordion").accordion({
+        active: false,
+        collapsible: true,
+        heightStyle: "content"
+    });
+});
--- _assets/js/header_shrink.js
+++ _assets/js/header_shrink.js
@@ -18,9 +18,11 @@ $(window).scroll(function(){
             if($('.site-header.homepage').data('size') == 'big')
             {
                 $('.site-header.homepage').data('size','small');
-                $('.site-header.homepage').stop().animate({ padding:'20px 0' }, { queue: false, duration: duration });
-                $('.site-header.homepage .trigger a.page-link').stop().animate({ fontSize:'14px' }, { queue: false, duration: duration });
+                // $('.site-header.homepage').stop().animate({ padding:'20px 0' }, { queue: false, duration: duration });
+                $('.site-header.homepage').stop().animate({ height:'70px' }, { queue: false, duration: duration });
+                $('.site-header.homepage .trigger a.page-link, .site-header.homepage a.page-anchor').stop().animate({ fontSize:'14px' }, { queue: false, duration: duration });
                 $('.site-header.homepage .site-nav .responsive-menu').stop().animate({paddingTop:'85px'}, { queue: false, duration: duration });
+                $('.site-header.homepage a.page-anchor').addClass('semSeta');
             }
         }
         else
@@ -28,14 +30,18 @@ $(window).scroll(function(){
             if($('.site-header.homepage').data('size') == 'small')
             {
                 $('.site-header.homepage').data('size','big');
-                $('.site-header.homepage').stop().animate({padding:'35px 0'}, { queue: false, duration: duration });
-                $('.site-header.homepage .trigger a.page-link').stop().animate({ fontSize:'18px' }, { queue: false, duration: duration });
+                // $('.site-header.homepage').stop().animate({padding:'35px 0'}, { queue: false, duration: duration });
+                $('.site-header.homepage').stop().animate({height:'100px'}, { queue: false, duration: duration });
+                $('.site-header.homepage .trigger a.page-link, .site-header.homepage a.page-anchor').stop().animate({ fontSize:'18px' }, { queue: false, duration: duration });
                 $('.site-header.homepage .site-nav .responsive-menu').stop().animate({paddingTop:'160px'}, { queue: false, duration: duration });
+                $('.site-header.homepage a.page-anchor').removeClass('semSeta');
             }  
         }
     }else{
-        $('.site-header.homepage .trigger a.page-link').stop().animate({ fontSize:'14px' }, { queue: false, duration: duration });
-        $('.site-header.homepage').stop().animate({ padding:'20px 0' }, { queue: false, duration: duration });
+        $('.site-header.homepage .trigger a.page-link, .site-header.homepage a.page-anchor').stop().animate({ fontSize:'14px' }, { queue: false, duration: duration });
+        // $('.site-header.homepage').stop().animate({ padding:'20px 0' }, { queue: false, duration: duration });
+        $('.site-header.homepage').stop().animate({ height:'70px' }, { queue: false, duration: duration });
         $('.site-header.homepage .site-nav .responsive-menu').stop().animate({paddingTop:'85px'}, { queue: false, duration: duration });        
+        $('.site-header.homepage a.page-anchor').addClass('semSeta');
     }
 });
